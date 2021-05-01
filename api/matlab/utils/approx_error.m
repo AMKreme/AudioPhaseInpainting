a1e85ce46884cf08e12f1b95dd32d8028604b920
Marina Kreme <amamarinak@gmail.com>
2021-05-01 19:31:21 +0200

    delete

--- api/matlab/utils/approx_error.m
+++
@@ -1,3 +0,0 @@
-function err = approx_error(X,x)
-err = norm(X-x*x','fro')./norm(X,'fro');
-end
--- api/matlab/utils/error_approx.m
+++
@@ -1,3 +0,0 @@
-function err =  error_approx(X_est, X_ref)
-err = 20*log10((norm(X_est-X_ref, 'fro'))./(norm(X_ref)));
-end
